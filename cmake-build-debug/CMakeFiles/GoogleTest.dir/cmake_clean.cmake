bf95c0f24b1ab6931a47139177a2d7fa7254ff18
isaaklindgren <isali@student.chalmers.se>
2021-01-28 10:36:36 +0100

    initial commit

---
+++ cmake-build-debug/CMakeFiles/GoogleTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GoogleTest.dir/main.cpp.o"
+  "GoogleTest"
+  "GoogleTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GoogleTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ unit_tests.cpp
@@ -0,0 +1,10 @@
+//
+// Created by isaaklindgren on 2021-01-28.
+//
+
+#include "gtest/gtest.h"
+
+
+TEST(SuiteOne, TestOne){
+    ASSERT_TRUE(1==1);
+}
